adcd26b06fc8a1a361cee27c1caff05c50fb5780
Shivam Sharma <sharmass1996@gmail.com>
2023-04-15 13:45:18 +0530

    quick update

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,8 +7,6 @@ function Login({setIsLoggedIn}) {
                 Sign Up
             </h2>
             <form>
-                Username: <input type="text" onChange={} />
-                Password: <input type="password" onChange={}></input>
                 
             </form>
         </div>
